b70d6bcde36fafef4200b805a0a0e088ac918d69
wtooo545 <borslajaloul@gmail.com>
2025-05-31 00:43:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
-        couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
+        buyer: '1339981018416808000', // Buyer
+        couleur: '#ebedf0', // Couleur HEX
+        footer: '[+] Wtoo', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
